e3389c2507e73292f8a68fd9edc112be5f685120
Daniel Rivers <me@danielrivers.com>
2025-09-16 23:48:11 +0000

    you shall not register

---
+++ kindeSrc/environment/workflows/preRegistration/Workflow.ts
@@ -0,0 +1,17 @@
+import {
+  onPostAuthenticationEvent,
+  WorkflowSettings,
+  WorkflowTrigger,
+  denyAccess
+} from "@kinde/infrastructure";
+
+// The setting for this workflow
+export const workflowSettings: WorkflowSettings = {
+  id: "onPostUserAuthentication",
+  trigger: WorkflowTrigger.UserPreRegistration,
+};
+
+// The workflow code to be executed when the event is triggered
+export default async function Workflow(event: onPostAuthenticationEvent) {
+  denyAccess("Everyone is denied!")
+}
