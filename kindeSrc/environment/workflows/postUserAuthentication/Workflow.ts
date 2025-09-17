9afe80cbfb9c718de924946b91a4b8f069689c49
Daniel Rivers <me@danielrivers.com>
2025-09-17 08:10:51 +0000

    heidi test

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -1,16 +1,50 @@
 import {
-  onPostAuthenticationEvent,
+  onUserTokenGeneratedEvent,
   WorkflowSettings,
   WorkflowTrigger,
+  accessTokenCustomClaims,
+  getEnvironmentVariable,
 } from "@kinde/infrastructure";
 
-// The setting for this workflow
+// TODO: change sso session config to non-persistent
+
 export const workflowSettings: WorkflowSettings = {
-  id: "onPostUserAuthentication",
-  trigger: WorkflowTrigger.PostAuthentication,
+  id: "nonPersistentSessionWorkflow",
+  name: "Non Persistent Session Workflow",
+  trigger: WorkflowTrigger.UserTokenGeneration,
+  bindings: {
+    "kinde.accessToken": {},
+    "kinde.ssoSession": {},
+    "kinde.env": {},
+  },
+  failurePolicy: {
+    action: "stop",
+  },
 };
 
-// The workflow code to be executed when the event is triggered
-export default async function Workflow(event: onPostAuthenticationEvent) {
-  console.log("Hello world");
+export default async function NonPersistentSessionWorkflow(
+  event: onUserTokenGeneratedEvent
+) {
+  let policy = "persistent";
+
+  const nonPersistentConnectionIDs = getEnvironmentVariable(
+    "NON_PERSISTENT_SESSION_CONNECTION_IDS"
+  ).value.split(",");
+
+  console.log("nonPersistentConnectionIDs:", nonPersistentConnectionIDs);
+  if (nonPersistentConnectionIDs.includes(event.context.auth.connectionId)) {
+    policy = "non_persistent";
+    console.log("setting sso session policy to non_persistent");
+    kinde.ssoSession.setPolicy(policy);
+    console.log("sso session policy set to non_persistent");
+  }
+
+  console.log("event: ", event);
+
+  const accessToken = accessTokenCustomClaims<{
+    isDeployed: boolean;
+    // ksp: string;
+  }>();
+  accessToken.isDeployed = true;
+  // accessToken.ksp = policy;
 }
