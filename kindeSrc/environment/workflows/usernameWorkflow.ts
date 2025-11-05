1cdf768c1aca45f99ebc5bb525fed0256949aed5
Daniel Rivers <me@danielrivers.com>
2025-11-05 11:24:53 +0000

    Clean up usernameWorkflow.ts by removing unused code
    Removed unused imports and interface definitions to clean up the code.

--- kindeSrc/environment/workflows/usernameWorkflow.ts
+++ kindeSrc/environment/workflows/usernameWorkflow.ts
@@ -1,9 +1,4 @@
 import {
-	WorkflowTrigger,
-	fetch,
-	secureFetch,
-	createKindeAPI,
-	getEnvironmentVariable,
 	invalidateFormField
 } from "@kinde/infrastructure";
 
@@ -16,26 +11,9 @@ export const workflowSettings = {
 	},
 	bindings: {
 		"kinde.widget": {}, // Required for accessing the UI
-		"kinde.env": {}, // required to access your environment variables
-		"kinde.fetch": {}, // Required for external and Kinde management API calls
-		"kinde.secureFetch": {}, // Required for external API calls
-		"url": {}, // required for url params
 	},
 };
 
-interface RopcResponse {
-	given_name?: string
-	family_name?: string
-	user_id: string
-}
-interface Tenant {
-	Id: string
-	Name: string
-	KindeOrganizationCode?: string
-}
-
-
 export default async function Workflow(event: any) {
-	console.log(event)
 	invalidateFormField('p_username', 'username format sucks, do better!')
 }
